3fa79b8833cda601e28b97dd9c0bf5fdbacc42aa
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-02-10 23:43:33 +0800

    Update B(op_sol).cpp

--- B(op_sol).cpp
+++ B(op_sol).cpp
@@ -7,6 +7,7 @@ int main() {
   cin.tie(0);
   int n, x;
   cin >> n >> x;
+  // view the image in this repository for better understanding
   int sum = 0;
   for (int i = 0; i < n; i++) {
   	int v, p;
